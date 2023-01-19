2ba3d2ea0572ea9f4dc105534374ae8ea2fd1c26
Roger Knight <roger@rogerknight.info>
2023-01-19 11:29:41 +0100

    Use Prettier to format code

--- cb-functionality.php
+++ cb-functionality.php
@@ -6,7 +6,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Curiosity Box Functions
- * Version:     1.3.2
+ * Version:     1.3.3
  * Plugin URI:  https://github.com/AttractMore/curiosity-box-functionality-plugin
  * Description: The collection of custom functions for the Curiosity Box website
  * Author:      AttractMore (Roger Knight)
@@ -21,25 +21,25 @@
  *
  */
 
- /**
+/**
  * @snippet Translate string Shopping Cart to Basket on Cart and Checkout
  * @source https://www.businessbloomer.com/translate-single-string-woocommerce-wordpress/
  * @source https://twenty-eighty.co.uk/resources/how-do-i-change-cart-to-basket-in-woocommerce/
  */
 
-add_filter( 'gettext', 'am_translate_cart_to_basket', 999, 3);
+add_filter("gettext", "am_translate_cart_to_basket", 999, 3);
 
 function am_translate_cart_to_basket($translated, $untranslated, $domain) {
- if ( ! is_admin() && 'flatsome' === $domain && ( is_cart() || is_checkout() ) ) {
-   $translated = str_ireplace( 'Shopping Cart', 'Basket', $translated );
- }
- return $translated;
+  if (!is_admin() && "flatsome" === $domain && (is_cart() || is_checkout())) {
+    $translated = str_ireplace("Shopping Cart", "Basket", $translated);
+  }
+  return $translated;
 }
 
 /**
  * @snippet Untick Deliver to different Address
-*/
-add_filter( 'woocommerce_ship_to_different_address_checked', '__return_false' );
+ */
+add_filter("woocommerce_ship_to_different_address_checked", "__return_false");
 
 /**
  * @snippet       Rename "Returning Customer?" @ Checkout Page - WooCommerce
@@ -47,7 +47,7 @@ add_filter( 'woocommerce_ship_to_different_address_checked', '__return_false' );
  * @author        Rodolfo Melogli
  */
 
-add_filter( 'woocommerce_checkout_login_message', 'am_return_customer_message' );
+add_filter("woocommerce_checkout_login_message", "am_return_customer_message");
 
 function am_return_customer_message() {
   return "If you're a new customer, please proceed to the Billing section below.<br>If you've shopped with us before, please ";
@@ -55,37 +55,44 @@ function am_return_customer_message() {
 
 /**
  * @snippet REMOVE ORDER NOTES
-*/
-add_filter( 'woocommerce_checkout_fields' , 'am_remove_order_notes' );
-function am_remove_order_notes( $fields ) {
-  unset($fields['order']['order_comments']);
+ * @return array
+ */
+add_filter("woocommerce_checkout_fields", "am_remove_order_notes");
+function am_remove_order_notes($fields) {
+  unset($fields["order"]["order_comments"]);
   return $fields;
 }
 
 /**
  *   REMOVE OPTIONAL TEXT NEXT TO FIELDS IN CHECKOUT
-*/
-add_filter( 'woocommerce_form_field' , 'am_remove_checkout_optional_text', 10, 4 );
-function am_remove_checkout_optional_text( $field, $key, $args, $value ) {
-  if( is_checkout() && ! is_wc_endpoint_url() ) {
-    $optional = '<span class="optional">(' . esc_html__( 'optional', 'woocommerce' ) . ')</span>';
-    $field = str_replace( $optional, '', $field );
+ */
+add_filter("woocommerce_form_field", "am_remove_checkout_optional_text", 10, 4);
+function am_remove_checkout_optional_text($field, $key, $args, $value) {
+  if (is_checkout() && !is_wc_endpoint_url()) {
+    $optional =
+      '<span class="optional">(' .
+      esc_html__("optional", "woocommerce") .
+      ")</span>";
+    $field = str_replace($optional, "", $field);
   }
   return $field;
 }
- /**
+/**
  * @source https://shanerutter.co.uk/fix-for-woocommerce-schema-data-missing-brand-and-mpn/
  * @source https://wp-tutorials.tech/refine-wordpress/add-brand-to-woocommerce-product-schema/
  * @snippet Add missing Rich Snippet  data not handled by WooCommerce yet
  */
-add_filter( 'woocommerce_structured_data_product', 'custom_woocommerce_structured_data_product' );
+add_filter(
+  "woocommerce_structured_data_product",
+  "custom_woocommerce_structured_data_product"
+);
 
-function custom_woocommerce_structured_data_product ($data) {
-	//global $product;
-  $data['brand'] = array('@type' => 'Brand', 'name' => 'Curiosity Box');
-//	$data['sku'] = $product->get_sku();
+function custom_woocommerce_structured_data_product($data) {
+  //global $product;
+  $data["brand"] = ["@type" => "Brand", "name" => "Curiosity Box"];
+  //	$data['sku'] = $product->get_sku();
 
-	return $data;
+  return $data;
 }
 
 /**
@@ -93,28 +100,28 @@ function custom_woocommerce_structured_data_product ($data) {
  * @author        Rodolfo Melogli
  * @compatible    WooCommerce 5
  */
- 
-add_filter( 'woocommerce_checkout_coupon_message', 'am_have_coupon_message');
- 
+
+add_filter("woocommerce_checkout_coupon_message", "am_have_coupon_message");
+
 function am_have_coupon_message() {
-   return '<strong>Have a coupon that\'s not been applied automatically? <a href="#" class="showcoupon">Enter your code</a></strong>';
+  return '<strong>Have a coupon that\'s not been applied automatically? <a href="#" class="showcoupon">Enter your code</a></strong>';
 }
 
 /**
  * @snippet REPLACE LOGIN HEADER
-*/
-add_filter( 'login_headertitle', 'am_change_login_page_title' );
+ */
+add_filter("login_headertitle", "am_change_login_page_title");
 
 function my_custom_login_logo() {
   echo '<style type="text/css"> #login h1 a { background-image:url(/wp-content/uploads/2021/08/cb-logo-300.png) !important; width:auto; background-size:85%; height:120px; margin-bottom:18px;} </style>';
 }
-add_action('login_head', 'my_custom_login_logo');
+add_action("login_head", "my_custom_login_logo");
 
 function am_change_login_page_url($login_header_url) {
-  return get_bloginfo( 'url' );
+  return get_bloginfo("url");
 }
-add_filter( 'login_headerurl', 'am_change_login_page_url' );
+add_filter("login_headerurl", "am_change_login_page_url");
 
 function am_change_login_page_title($login_header_title) {
-   return get_bloginfo('title');
+  return get_bloginfo("title");
 }
---
+++ readme.md
@@ -0,0 +1,48 @@
+# Curiosity Box Functionality Plugin
+
+Contributors: roger knight
+License: GPLv3
+Tags: Woocommerce
+Tested up to: 6.1
+Stable tag: 1.3.3
+Requires PHP: 5.6.20
+
+Various functions for Curiosity Box
+
+## Description
+
+Curiosity Box Functionality Plugin - a ratatouille of functions
+
+This plugin contains a variety of functions and filters required for the proper functioning of the Curiosity Box website.
+
+## Changelog
+
+### 1.3.3
+
+Release date: 19 January 2023
+
+Formatted code with Prettier
+
+### 1.3
+
+Release date: 8 December 2022
+
+Moved more code over from functions.php
+
+### 1.2
+
+Release date: 2 December 2022
+
+Remove mini cart code as not required or necessary
+
+## 1.1
+
+Release date: 2 December 2022
+
+Add code to translate text on mini cart button
+
+## 1.0
+
+Release date: 2 December 2022
+
+Initial release with limited functionality
